b752ae6a27c7d37380fc9d1502d98543431e1536
Eduardo Almachi <108561115+edusebass@users.noreply.github.com>
2023-04-12 15:58:35 -0500

    Update 100 - iterators.py

--- roadmap/100 - iterators.py
+++ roadmap/100 - iterators.py
@@ -1,1 +1,9 @@
+/*
+Iterator in Python is an object that is used to iterate over iterable objects like lists, tuples, dicts, and sets. 
+The iterator object is initialized using the iter() method. It uses the next() method for iteration.
+
+__iter__(): The iter() method is called for the initialization of an iterator. This returns an iterator object
+__next__(): The next method returns the next value for the iterable. When we use a for loop to traverse any iterable object, internally it uses the iter() method 
+  to get an iterator object, which further uses the next() method to iterate over. This method raises a StopIteration to signal the end of the iteration.
+*/
 
